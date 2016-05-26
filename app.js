26d7869f90e225eb9b774f872407d64f3af09440
Marko Bodi <bodimarko@outlook.com>
2016-05-25 21:08:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// var userName= prompt ('What\'s your name?');
+// alert("Welcome " + userName);
+var questionOne = prompt('What country was I born in?').toLowerCase();
+if (questionOne === 'serbia') {
+alert('Yay, you know me so well! \n Gold Star!');
+} else {
+alert('Wrong!')
+}
